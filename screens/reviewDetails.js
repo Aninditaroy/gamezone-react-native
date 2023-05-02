1e1097913ef5b7ee8f3b95af3bdfd0080d271cda
Aninditaroy <anindita.roy.mu@gmail.com>
2023-05-02 20:45:47 +0600

    custom card and dynamic rating images added

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/card';
 
 const ReviewDetails = ({ navigation, route }) => {
-
+  const rating = route.params.item.rating;
   return (
     <View style={globalStyles.container}>
-      <Text>{route.params.item.title}</Text>
-      <Text>{route.params.item.body}</Text>
-      <Text>{route.params.item.rating}</Text>
+      <Card>
+        <Text>{route.params.item.title}</Text>
+        <Text>{route.params.item.body}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image
+            source={images.ratings[rating]}
+          />
+        </View>
+      </Card>
     </View>
   )
 }
 
 export default ReviewDetails;
 
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  }
+})
